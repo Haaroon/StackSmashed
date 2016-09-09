812c0597f6d711a53d8083b31b3b0143a8634180
Haaroon <haaroon.yousaf@gmail.com>
2016-09-09 16:52:22 +0100

    added clear, enter

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-int name=0;
+char name[9];
+
+/* Wait for enter command before continuing */
+int enterToContinue(){
+	printf("> Press enter to continue\n>>");
+	char enter = 0;
+	while (enter != '\r' && enter != '\n') { enter = getchar(); }
+}
+
+/* clear the damn screen */
+void clearScreen() {
+  const char* CLEAR_SCREE_ANSI = "\e[1;1H\e[2J";
+  write(STDOUT_FILENO,CLEAR_SCREE_ANSI,12);
+}
 
 /* Get name from user */
-int setName(){
-	char name[9];
-	printf("Please enter your name (Max 8 characters) \nbelow followed by [Enter] key:\n");
+static int setName(){
+	printf("> Please enter your name (Max 8 characters) \n> below followed by [Enter] key:\n>> ");
 	fgets(name,10,stdin);
 	// remove new line har
 	name[ strlen(name) - 1 ] = '\0';
-	printf("\nWelcome %s to the game! \n", name);
+	printf("\n> Welcome %s to the game! \n", name);
+	enterToContinue();
+	clearScreen();
 }
 
 int loadSaveGame(){
@@ -22,22 +37,30 @@ int saveGame(){
 }
 
 /* starting message */
-int welcome(){
+static int welcome(){
 	printf("********************\n");
-	printf("Welcome to my text based game\n");
+	printf("> Welcome to my text based game\n");
 	//TODO Add a check for the savegame here
 	setName();
-	printf("**Please follow instructions carefully to progress\n");
-	printf("**Please type the letter h for the help menu\n");
-	printf("**This game can only be played using a keyboard\n");
-	printf("**At any time you may save by typing s then enter\n");
+	printf("> Please follow instructions carefully to progress\n");
+	printf("> Lines starting with > are informational\n");
+	printf("> Lines starting with >> expect your input\n");
+	printf("> Enter h for the help menu\n");
+	sleep(1);
+	printf("> This game can only be played using a keyboard\n");
+	printf("> Enter s to save\n");
+	sleep(1);
+	enterToContinue();
 	printf("********************\n");
 	return 0;
 }
 
-
+/* Main method, duhh */
 int main() {
-
 	welcome();
-
+	printf("> %s, You wake up, all wet and flustered.\n", name);
+	printf("> Its dark, it seems you have been injured but patched up.\n");
+	//while(getInput() && parseCommand());
+	printf("> Come again!\n");
+	return 0;
 }
